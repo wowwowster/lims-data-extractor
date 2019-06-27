0596823739682f76b15d64f6bddcebfb9b770b3e
carl.laurier <carl.laurier@telino.com>
2019-06-27 16:47:31 +0200

    Initial commit

---
+++ src/main/java/com/claurier/limsdataextractor/LimsDataExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.claurier.limsdataextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimsDataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimsDataExtractorApplication.class, args);
+	}
+
+}
